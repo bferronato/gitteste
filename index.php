f7714636ca9e1e5df94b0eeab10659678ccce972
Bruno <bferronato@gmail.com>
2016-11-28 23:27:52 -0200

    Update index.php

--- index.php
+++ index.php
@@ -55,9 +55,6 @@
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57958acf666deb78"></script>
 	-->
 	
-	<script src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5188f1240ece93e4" type="text/javascript"></script>
-
-	
 	<!-- 
 	Go to www.addthis.com/dashboard to customize your tools 
 	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57958acf666deb78"></script>
