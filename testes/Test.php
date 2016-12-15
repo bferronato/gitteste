27a4ae4579359886476082d11d7fc1ec81159967
Bruno <bferronato@gmail.com>
2016-12-15 01:13:56 -0200

    Update Test.php

--- testes/Test.php
+++ testes/Test.php
@@ -4,5 +4,9 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+	
+	public function testFourPlusFour() {
+		$this->assertEquals(4+4,8);
+  	}
 }
 ?>
