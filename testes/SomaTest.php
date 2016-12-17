e319b4504e82ab49bdb650bec6984f3df812d7c6
Bruno <bferronato@gmail.com>
2016-12-17 00:41:49 -0200

    Update SomaTest.php

--- testes/SomaTest.php
+++ testes/SomaTest.php
@@ -5,6 +5,9 @@ class Test extends PHPUnit_Framework_TestCase
 		$this->assertEquals(1+1,2);
   	}
 	
+	/**
+     * @depends testEmpty
+     */
 	public function testFourPlusFour() {
 		$this->assertEquals(4+4,8);
   	}
